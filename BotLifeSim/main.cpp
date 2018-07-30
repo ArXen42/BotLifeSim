45f2072071e28644eab73b6167e16eadd00daf13
arxen42 <arxen42@tutanota.com>
2018-07-30 23:30:02 +0300

    RenderWindow, code style

--- BotLifeSim/main.cpp
+++ BotLifeSim/main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
-#include "SFML/Window.hpp"
+#include "SFML/Graphics.hpp"
 
-int main() {
-    sf::Window window(sf::VideoMode(600, 500), "Bot life sim");
+int main()
+{
+	sf::RenderWindow window(sf::VideoMode(600, 500), "Bot life sim");
 
-    // run the program as long as the window is open
-    while (window.isOpen()) {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-    }
+	while (window.isOpen())
+	{
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
 
-    return 0;
+		window.clear(sf::Color::Black);
+
+		// draw everything here...
+		// window.draw(...);
+
+		// end the current frame
+		window.display();
+	}
+
+	return 0;
 }
