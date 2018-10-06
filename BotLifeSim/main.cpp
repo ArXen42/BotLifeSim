a4e838df930665e6e6c935766552f12b38aae789
arxen42 <arxen42@tutanota.com>
2018-10-07 00:49:33 +0300

    Prepare SFML window

--- BotLifeSim/main.cpp
+++ BotLifeSim/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
 #include "SFML/Graphics.hpp"
 
+constexpr sf::Uint32 width = 1280, height = 720;
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(600, 500), "Bot life sim");
+	sf::RenderWindow window(sf::VideoMode(width, height), "Bot life sim");
+
+	sf::Texture texture;
+	texture.create(width, height);
+	texture.setSmooth(false);
+
+	sf::Image image{};
+	image.create(width, height);
+
+	for (sf::Uint32 i = 0; i < width; i++)
+	{
+		image.setPixel(i, height / 2, sf::Color::Red);
+	}
+	texture.update(image);
+
+	sf::Sprite sprite;
+	sprite.setTexture(texture);
 
 	while (window.isOpen())
 	{
@@ -15,6 +33,7 @@ int main()
 		}
 
 		window.clear(sf::Color::Black);
+		window.draw(sprite);
 
 		// draw everything here...
 		// window.draw(...);
